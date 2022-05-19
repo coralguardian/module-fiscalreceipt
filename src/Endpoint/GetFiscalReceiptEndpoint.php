7474b17464a2fbc7fac02c375a7e697bb4ae1b53
d4rk0s <gregory@d4rk0s.net>
2022-05-19 22:29:37 +0200

    bugfix

--- src/Endpoint/GetFiscalReceiptEndpoint.php
+++ src/Endpoint/GetFiscalReceiptEndpoint.php
@@ -3,6 +3,9 @@
 namespace D4rk0snet\FiscalReceipt\Endpoint;
 
 use D4rk0snet\Adoption\Entity\AdoptionEntity;
+use D4rk0snet\Coralguardian\Entity\CompanyCustomerEntity;
+use D4rk0snet\Coralguardian\Entity\IndividualCustomerEntity;
+use D4rk0snet\Donation\Entity\DonationEntity;
 use D4rk0snet\FiscalReceipt\Service\FiscalReceiptService;
 use D4rk0snet\FiscalReceipt\Model\FiscalReceiptModel;
 use Hyperion\Doctrine\Service\DoctrineService;
@@ -23,26 +26,46 @@ class GetFiscalReceiptEndpoint extends APIEnpointAbstract
         }
 
         // @todo : faire la recherche ensuite dans les dons si non trouvé
-        /** @var AdoptionEntity $order */
-        $order = DoctrineService::getEntityManager()->getRepository(AdoptionEntity::class)->find($orderUUID);
+        /** @var DonationEntity $order */
+        $order = DoctrineService::getEntityManager()->getRepository(DonationEntity::class)->find($orderUUID);
         if ($order === null) {
             return APIManagement::APIError('Order not found', 404);
         }
 
-        $fiscalReceiptModel = new FiscalReceiptModel(
-            articles: '45/407',
-            receiptCode: 1,
-            customerFullName: $order->getFirstname(). " ".$order->getLastname(),
-            customerAddress: $order->getAddress(),
-            customerPostalCode: "xxx",
-            customerCity: $order->getCity(),
-            fiscalReductionPercentage: 60,
-            priceWord: "soixante",
-            price: $order->getAmount(),
-            date: new \DateTime(),
-            orderUuid: $orderUUID,
-            paymentMethod: 'Carte bancaire'
-        );
+        $customer = $order->getCustomer();
+
+        if($customer instanceof IndividualCustomerEntity) {
+            $fiscalReceiptModel = new FiscalReceiptModel(
+                articles: '45/407',
+                receiptCode: 1,
+                customerFullName: $customer->getFirstname() . " " . $customer->getLastname(),
+                customerAddress: $customer->getAddress(),
+                customerPostalCode: "xxx",
+                customerCity: $customer->getCity(),
+                fiscalReductionPercentage: 60,
+                paymentMethod: 'Carte bancaire',
+                priceWord: "soixante",
+                price: $order->getAmount(),
+                date: new \DateTime(),
+                orderUuid: $orderUUID
+            );
+        } else {
+            /** @var CompanyCustomerEntity $customer */
+            $fiscalReceiptModel = new FiscalReceiptModel(
+                articles: '45/407',
+                receiptCode: 1,
+                customerFullName: $customer->getCompanyName(),
+                customerAddress: $customer->getAddress(),
+                customerPostalCode: "xxx",
+                customerCity: $customer->getCity(),
+                fiscalReductionPercentage: 60,
+                paymentMethod: 'Carte bancaire',
+                priceWord: "soixante",
+                price: $order->getAmount(),
+                date: new \DateTime(),
+                orderUuid: $orderUUID
+            );
+        }
 
         $fileURL = FiscalReceiptService::createReceipt($fiscalReceiptModel);
 
